3e0e0c33d32dcaf601a169065afa025a01974328
Renata C R <crrenatacr@gmail.com>
2024-05-09 11:02:00 +0200

    DocumentationAndIndexUpdate

--- index.js
+++ index.js
@@ -25,19 +25,60 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // GET route for /
 app.get('/', (req, res) => {
-    // Return a default textual response
     res.send('Welcome to our movie API! Visit /movies to get data about the top 10 most watched movies.');
 });
 
 // GET route for /movies
 app.get('/movies', (req, res) => {
-    // Return the top 10 most watched movies as JSON
     res.json(moviesData);
 });
 
-// GET route for serving the documentation.html file
-app.get('/documentation', (req, res) => {
-    res.sendFile('documentation.html', { root: path.join(__dirname, 'public') });
+// GET route for /movies/:title
+app.get('/movies/:title', (req, res) => {
+    const title = req.params.title;
+    res.send(`You requested data about the movie with title: ${title}`);
+});
+
+// GET route for /genres/:name
+app.get('/genres/:name', (req, res) => {
+    const name = req.params.name;
+    res.send(`You requested data about the genre with name: ${name}`);
+});
+
+// GET route for /directors/:name
+app.get('/directors/:name', (req, res) => {
+    const name = req.params.name;
+    res.send(`You requested data about the director with name: ${name}`);
+});
+
+// POST route for /users/register
+app.post('/users/register', (req, res) => {
+    res.send('User registration successful');
+});
+
+// PUT route for /users/:userId
+app.put('/users/:userId', (req, res) => {
+    const userId = req.params.userId;
+    res.send(`User with ID ${userId} updated successfully`);
+});
+
+// POST route for /users/:userId/favorites
+app.post('/users/:userId/favorites', (req, res) => {
+    const userId = req.params.userId;
+    res.send(`Movie added to favorites for user with ID ${userId}`);
+});
+
+// DELETE route for /users/:userId/favorites/:movieId
+app.delete('/users/:userId/favorites/:movieId', (req, res) => {
+    const userId = req.params.userId;
+    const movieId = req.params.movieId;
+    res.send(`Movie removed from favorites for user with ID ${userId}`);
+});
+
+// DELETE route for /users/:userId
+app.delete('/users/:userId', (req, res) => {
+    const userId = req.params.userId;
+    res.send(`User with ID ${userId} deregistered successfully`);
 });
 
 // Error-handling middleware function to log application-level errors
