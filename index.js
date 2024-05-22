e0b089d147b8356b841db5bfe3be6c83269b31cb
Renata C R <crrenatacr@gmail.com>
2024-05-22 17:15:53 +0200

    AuthEndpointsIndexUpdate

--- index.js
+++ index.js
@@ -19,24 +19,31 @@ app.use(express.static(path.join(__dirname, "public"))); // Serve static files
 // Autenticação
 let auth = require('./auth')(app); // Adicionado para importar e inicializar auth
 
+// Import and initialize Passport for authentication
+const passport = require('passport'); // Authentication middleware
+require('./passport'); // Configuration of authentication strategies
+
 // GET route for /
 app.get("/", (req, res) => {
     res.send("Welcome to our movie API!");
 });
 
 // GET route for /movies
-app.get("/movies", (req, res, next) => {
-    Movies.find()
-        .then(movies => {
-            res.json(movies);
-        })
-        .catch(next);
+app.get("/movies", passport.authenticate('jwt', { session: false }), async (req, res) => {
+  await Movies.find()
+    .then((movies) => {
+      res.status(201).json(movies);
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send('Error: ' + error);
+    });
 });
 
 // GET route for /movies/:title
-app.get("/movies/:title", (req, res, next) => {
+app.get("/movies/:title", passport.authenticate('jwt', { session: false }), async (req, res, next) => {
     const title = req.params.title;
-    Movies.findOne({ Title: title })
+    await Movies.findOne({ Title: title })
         .then(movie => {
             if (!movie) {
                 return res.status(404).send("Movie not found");
@@ -62,8 +69,8 @@ app.post("/users/register", (req, res, next) => {
 });
 
 // GET route for listing all users
-app.get("/users", (req, res, next) => {
-  Users.find()
+app.get("/users", passport.authenticate('jwt', { session: false }), async (req, res, next) => {
+  await Users.find()
       .then(users => {
           res.json(users);
       })
@@ -71,11 +78,11 @@ app.get("/users", (req, res, next) => {
 });
 
 // PUT route for updating user info
-app.put("/users/:userId", (req, res, next) => {
+app.put("/users/:userId", passport.authenticate('jwt', { session: false }), async (req, res, next) => {
     const userId = req.params.userId;
     const updatedUserData = req.body;
 
-    Users.findByIdAndUpdate(userId, updatedUserData, { new: true })
+    await Users.findByIdAndUpdate(userId, updatedUserData, { new: true })
         .then(user => {
             if (!user) {
                 return res.status(404).send("User not found");
@@ -86,7 +93,7 @@ app.put("/users/:userId", (req, res, next) => {
 });
 
 // POST route for adding a movie to favorites
-app.post("/users/:userId/favorites", (req, res) => {
+app.post("/users/:userId/favorites", passport.authenticate('jwt', { session: false }), (req, res) => {
   const userId = req.params.userId;
   const movieId = req.body.movieId;
 
@@ -108,10 +115,10 @@ app.post("/users/:userId/favorites", (req, res) => {
 });
 
 // DELETE route for user deregistration
-app.delete("/users/:userId", (req, res, next) => {
+app.delete("/users/:userId", passport.authenticate('jwt', { session: false }), async (req, res, next) => {
     const userId = req.params.userId;
 
-    Users.findByIdAndDelete(userId)
+    await Users.findByIdAndDelete(userId)
         .then(user => {
             if (!user) {
                 return res.status(404).send("User not found");
@@ -122,11 +129,11 @@ app.delete("/users/:userId", (req, res, next) => {
 });
 
 // DELETE route for removing a movie from favorites
-app.delete("/users/:userId/favorites/:movieId", (req, res, next) => {
+app.delete("/users/:userId/favorites/:movieId", passport.authenticate('jwt', { session: false }), async (req, res, next) => {
   const userId = req.params.userId;
   const movieId = req.params.movieId;
 
-  Users.findByIdAndUpdate(
+  await Users.findByIdAndUpdate(
     userId,
     { $pull: { FavoriteMovies: movieId } },
     { new: true }
