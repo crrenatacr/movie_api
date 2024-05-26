53ec85dc6bdd857ee82bd3bbfd92c2984f1128be
Renata C R <crrenatacr@gmail.com>
2024-05-26 13:02:28 +0200

    AddCorsIndex

--- index.js
+++ index.js
@@ -19,6 +19,21 @@ app.use(express.static(path.join(__dirname, "public"))); // Serve static files
 // Authentication
 let auth = require('./auth')(app); // Added to import and initialize auth
 
+const cors = require('cors'); 
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
+
+// Using CORS within the application
+app.use(cors({
+  origin: (origin, callback) => {
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
+      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+      return callback(new Error(message ), false);
+    }
+    return callback(null, true);
+  }
+})); 
+
 // Import and initialize Passport for authentication
 const passport = require('passport'); // Authentication middleware
 require('./passport'); // Configuration of authentication strategies
